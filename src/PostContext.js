e1636c3c5d9edfc490ec6da46e920fbb735bc92c
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-10-04 23:21:49 +0330

    Handled :: waisted renders by optimizing context

--- src/PostContext.js
+++ src/PostContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -21,7 +21,7 @@ function PostProvider({ children }) {
   // Derived state. These are the posts that will actually be displayed
   const searchedPosts =
     searchQuery.length > 0
-      ? posts.filter(post =>
+      ? posts.filter((post) =>
           `${post.title} ${post.body}`
             .toLowerCase()
             .includes(searchQuery.toLowerCase()),
@@ -29,23 +29,27 @@ function PostProvider({ children }) {
       : posts;
 
   function handleAddPost(post) {
-    setPosts(posts => [post, ...posts]);
+    setPosts((posts) => [post, ...posts]);
   }
 
   function handleClearPosts() {
     setPosts([]);
   }
 
+  // * Let's specify some optimization for
+  // * our context pattern
+  const value = useMemo(() => {
+    return {
+      posts: searchedPosts,
+      onClearPosts: handleClearPosts,
+      onAddPost: handleAddPost,
+      searchQuery,
+      setSearchQuery,
+    };
+  }, [searchQuery, searchedPosts]);
+
   return (
-    <PostContext.Provider
-      value={{
-        posts: searchedPosts,
-        onClearPosts: handleClearPosts,
-        onAddPost: handleAddPost,
-        searchQuery,
-        setSearchQuery,
-      }}
-    >
+    <PostContext.Provider value={value}>
       {children}
     </PostContext.Provider>
   );
